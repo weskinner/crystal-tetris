672994ad96682c84c99f914296bf5ecec4765963
William Skinner <william.skinner@chiprewards.com>
2015-02-24 08:22:43 -0600

    startup working

---
+++ src/graphics.cr
@@ -0,0 +1,58 @@
+module Tetris
+  WINDOW_TITLE = "tetris-sdl-c"
+
+  # a block 'pixel' of a playing field is 15px by 15px in size
+  BLOCK_SIZE = 20
+
+  # standard size of a tetris playing field
+  PLAYFIELD_HEIGHT = 22
+  PLAYFIELD_WIDTH = 10
+
+  WINDOW_HEIGHT = 22 * ( 20 + 1) + 1
+  WINDOW_WIDTH = 22 * ( 20 + 1) + 1
+
+  class Graphics
+    def initialize
+      @render_changed = false
+
+      @window = LibSDL2.create_window(WINDOW_TITLE, LibSDL2::WINDOWPOS_CENTERED, LibSDL2::WINDOWPOS_CENTERED,
+          WINDOW_WIDTH, WINDOW_HEIGHT, LibSDL2::WINDOW_SHOWN)
+
+      if @window == nil
+        raise "\nSDL_CreateWindow Error:  %s\n" + String.new(LibSDL2.get_error)
+      end
+
+      # Create a renderer that will draw to the window, -1 specifies that we want to load whichever
+      # video driver supports the flags we're passing
+      #
+      # Flags:
+      # SDL_RENDERER_ACCELERATED: We want to use hardware accelerated rendering
+      # SDL_RENDERER_PRESENTVSYNC: We want the renderer's present function (update screen) to be
+      # synchornized with the monitor's refresh rate
+      @render = LibSDL2.create_renderer(@window, -1, LibSDL2::RENDERER_ACCELERATED | LibSDL2::RENDERER_PRESENTVSYNC | LibSDL2::RENDERER_TARGETTEXTURE)
+
+      if @render == nil
+        raise "\nSDL_CreateRenderer Error:  %s\n" + String.new(LibSDL2.get_error)
+      end
+
+      LibSDL2.renderer_set_blend_mode(@render, LibSDL2::BlendMode::BLEND)
+
+      # texture for render context
+      @display = LibSDL2.create_texture(@render, 0x16462004_u32, LibSDL2::TextureAccess::TARGET, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+      LibSDL2.set_render_target(@render, @display)
+
+      # Load font
+      @font = LibSDL2_TTF.open_font("Ubuntu-M.ttf", 20)
+
+      if @font == nil
+        raise "\nTTF_OpenFont Error:  %s\n" + String.new(LibSDL2.get_error)
+      end
+    end
+
+    def finalize
+      LibSDL2.destroy_renderer(@render)
+      LibSDL2.destroy_window(@window)
+    end
+  end
+end
--- src/main.cr
+++ src/main.cr
@@ -1,44 +1,11 @@
 require "SDL2-Crystal/sdl2"
+require "./*"
 
-#    // Start up SDL, and make sure it went ok
-#    //
-#    uint32_t flags = SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_EVENTS;
-#    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-#
-#        fprintf(stderr,
-#                "\nUnable to initialize SDL:  %s\n",
-#                SDL_GetError());
-#
-#        return 1;
-#    }
-#
-#    atexit(cleanup);
-#
-#    if(TTF_Init() == -1) {
-#        fprintf(stderr,
-#                "\nTTF_Init Error:  %s\n",
-#                SDL_GetError());
-#        exit(1);
-#    }
-#
-#    init_graphics();
-#
-#    initTetris();
-#
-#    bool quit = false;
-#    while(!quit) {
-#
-#        preRender();
-#
-#        getInput();
-#
-#        updateTetris();
-#
-#        updateRender();
-#
-#        // Set to ~60 fps.
-#        // 1000 ms/ 60 fps = 1/16 s^2/frame
-#        SDL_Delay(16);
-#    }
+raise "Unable to init SDL " + String.new(LibSDL2.get_error) unless LibSDL2.init(LibSDL2::INIT_EVERYTHING)
+raise "TTF_Init Error: " + String.new(LibSDL2.get_error) unless LibSDL2_TTF.init() != -1
+Tetris::Graphics.new
 
-flags = LibSDL2::INIT_TIMER | LibSDL2::INIT_VIDEO | LibSDL2::INIT_EVENTS
+LibSDL2.delay 2000_u32
+
+LibSDL2_TTF.quit
+LibSDL2.quit
